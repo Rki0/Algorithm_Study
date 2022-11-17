1d90327cf9ca54be58bb25b93345ecd2efab908b
Rki0 <pky00823@gmail.com>
2022-11-17 18:17:57 +0900

    after delete

--- module_prac.js
+++ module_prac.js
@@ -1,29 +1,3 @@
-function solution(polynomial) {
-  const polyArr = polynomial.split(" + ");
+const test = "1,2,3,4,5,6";
 
-  let xNum = 0;
-  let num = 0;
-
-  polyArr.forEach((item) => {
-    if (item.includes("x")) {
-      const xArr = item.split("x");
-      console.log(xArr);
-
-      if (item[0] === "") {
-        xNum += 1;
-      }
-
-      if (item[0] !== "") {
-        xNum += Number(xArr[0]);
-      }
-    }
-
-    if (!item.includes("x")) {
-      num += Number(item);
-    }
-  });
-
-  return `${xNum}x + ${num}`;
-}
-
-console.log(solution("3x + 7 + x"));
+console.log(parseNumbers(test));
